3927c314709d7f509b1feed8b6ca93f6a44641ca
dankito <git@dankito.net>
2025-02-27 11:20:54 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+
+allprojects {
+    group = "net.codinux.pdf"
+    version = "1.0.0-SNAPSHOT"
+
+    repositories {
+        mavenCentral()
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    val kotlinVersion: String by settings
+
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+
+    plugins {
+        kotlin("jvm") version kotlinVersion apply false
+        kotlin("multiplatform") version kotlinVersion apply false
+
+        kotlin("plugin.allopen") version kotlinVersion apply false
+        kotlin("plugin.noarg") version kotlinVersion apply false
+
+        kotlin("plugin.serialization") version kotlinVersion apply false
+    }
+}
+
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
+}
