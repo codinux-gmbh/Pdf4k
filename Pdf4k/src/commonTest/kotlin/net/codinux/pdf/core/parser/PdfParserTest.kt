d43bfc1935db2ae9b54edd6c18074afb7c1b8dab
dankito <git@dankito.net>
2025-02-28 17:32:36 +0100

    Added test for trailer dictionary (at least for classical PDFs without Xref streams)

--- Pdf4k/src/commonTest/kotlin/net/codinux/pdf/core/parser/PdfParserTest.kt
+++ Pdf4k/src/commonTest/kotlin/net/codinux/pdf/core/parser/PdfParserTest.kt
@@ -1,8 +1,10 @@
 package net.codinux.pdf.core.parser
 
 import assertk.assertThat
-import assertk.assertions.isEqualTo
-import assertk.assertions.isNotNull
+import assertk.assertions.*
+import net.codinux.pdf.core.objects.PdfArray
+import net.codinux.pdf.core.objects.PdfHexString
+import net.codinux.pdf.core.objects.PdfRef
 import net.codinux.pdf.test.PdfTestData
 import kotlin.test.Test
 
@@ -28,6 +30,32 @@ class PdfParserTest {
         assertThat(result.header).isNotNull()
         assertThat(result.header!!.major).isEqualTo(1)
         assertThat(result.header!!.minor).isEqualTo(4)
+    }
+
+
+    @Test
+    fun parseTrailerDictionary_Pdf_1_4() {
+        val underTest = PdfParser(PdfTestData.Empty_v1_4_Uncompressed)
+
+        val result = underTest.parseDocument()
+
+        assertThat(result.trailerInfo).isNotNull()
+        // this PDF's trailer dictionary contains only /Root and /ID
+        assertThat(result.trailerInfo!!.root is PdfRef).isTrue()
+        assertThat((result.trailerInfo!!.root as PdfRef).objectNumber).isEqualTo(1)
+
+        assertThat(result.trailerInfo!!.info).isNull()
+        assertThat(result.trailerInfo!!.encrypt).isNull()
+
+        assertThat(result.trailerInfo!!.id is PdfArray).isTrue()
+        val idItems = (result.trailerInfo!!.id as PdfArray).items
+        assertThat(idItems).hasSize(2)
+        val creationHash = idItems[0]
+        val lastModifiedHash = idItems[1]
+        assertThat(creationHash is PdfHexString).isTrue()
+        assertThat((creationHash as PdfHexString).value).isEqualTo("3E8D5A4F84D0B77955DA8E168A9055E7")
+        assertThat(lastModifiedHash is PdfHexString).isTrue()
+        assertThat((lastModifiedHash as PdfHexString).value).isEqualTo("3E8D5A4F84D0B77955DA8E168A9055E7")
     }
 
 }
